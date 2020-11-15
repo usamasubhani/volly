4f942d972f97cb1b3846ce9ea3241ce059d5ddb0
Usama Subhani <reachusama@protonmail.com>
2020-11-15 16:29:37 +0500

    Show frozen lolly

---
+++ src/pages/frozen.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useQuery, useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import Lolly from '../components/lolly'
+
+const GetLollyBySlugQuery = gql`
+query GetLollyBySlug($slug: String!) {
+    GetLollyBySlug(slug: $slug) {
+      to
+      message
+      from
+      flavourTop
+      flavourMiddle
+      flavourBottom
+    }
+  }`
+
+const Frozen = ({ path, slug = "testslug", location }) => {
+    const { loading, error, data } = useQuery(GetLollyBySlugQuery, {
+        variables: {
+            slug: slug,
+        },
+    });
+
+    console.log(error)
+    console.log(data)
+
+    return (
+        <div>
+            <Lolly
+                flavourTop={data?.GetLollyBySlug?.flavourTop}
+                flavourMiddle={data?.GetLollyBySlug?.flavourMiddle}
+                flavourBottom={data?.GetLollyBySlug?.flavourBottom}
+            />
+            <div>
+                <p>To: {data?.GetLollyBySlug?.to}</p>
+                <p>From: {data?.GetLollyBySlug?.from}</p>
+                <p>Message: {data?.GetLollyBySlug?.message}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Frozen
