83ba000be66cd803d2f4c852757d6b2f1ac57642
Usama Subhani <reachusama@protonmail.com>
2020-11-15 19:18:10 +0500

    update function name

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: '/.netlify/functions/lolly',
+    uri: '/.netlify/functions/volly',
     fetch,
   }),
   cache: new InMemoryCache()
