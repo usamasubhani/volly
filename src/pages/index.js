b5feb7edd3b84a91c936d2948b50330541d8c109
Usama Subhani <reachusama@protonmail.com>
2020-11-20 00:36:11 +0500

    Add header and button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import { navigate } from "gatsby"
 import React from "react"
 import Lolly from '../components/lolly'
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <div>
       <header>
+        <Navbar />
+        <h1>Volly</h1>
+        <p>Send virtual lolly to someone</p>
         <section>
           <aside>
             <Lolly
@@ -30,9 +34,13 @@ export default function Home() {
           </aside>
         </section>
 
-        <button onClick={() => {
-          navigate("/createlolly")
-        }}>Create</button>
+        <p>
+          <a href="#" onClick={() => {
+            navigate("/createlolly")
+          }}>
+            <i>Create</i>
+          </a>
+        </p>
       </header>
     </div>
   )
