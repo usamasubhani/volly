45fe5551dad08420c11cdde35f4a4d94c13884b7
Usama Subhani <reachusama@protonmail.com>
2020-11-20 00:02:02 +0500

    Style createlolly page

--- src/pages/createlolly.jsx
+++ src/pages/createlolly.jsx
@@ -24,30 +24,30 @@ mutation createLolly(
         ) {slug}
     }`
 
-    const ErrorMsg = (props) => {
-        return (
-          <div>
+const ErrorMsg = (props) => {
+    return (
+        <div>
             <p className="errorMsg">{props.children}</p>
-          </div>
-        );
-      };
-    const initialValues = {
-        to: "",
-        message: "",
-        from: "",
-      };
-      
-      const validationSchema = Yup.object({
-        to: Yup.string().required("Recipient name is required"),
-      
-        message: Yup.string()
-          .required("Message is required")
-          .max(500, "Message should be less than 500 character"),
-        from: Yup.string().required("Sender name is Required"),
-      });
+        </div>
+    );
+};
+const initialValues = {
+    to: "",
+    message: "",
+    from: "",
+};
+
+const validationSchema = Yup.object({
+    to: Yup.string().required("Recipient name is required"),
+
+    message: Yup.string()
+        .required("Message is required")
+        .max(500, "Message should be less than 500 character"),
+    from: Yup.string().required("Sender name is Required"),
+});
 
 const CreateLolly = () => {
-    const [createLolly, {loading}] = useMutation(CreateLollyMutation);
+    const [createLolly, { loading }] = useMutation(CreateLollyMutation);
     const [flavours, setFlavours] = useState({
         flavourTop: "#A4193B",
         flavourMiddle: "#DF4343",
@@ -56,135 +56,145 @@ const CreateLolly = () => {
 
     const onSubmit = async (values, actions) => {
         const result = await createLolly({
-          variables: {
-            to: values.to,
-            message: values.message,
-            from: values.from,
-            flavourTop: flavours.flavourTop,
-            flavourMiddle: flavours.flavourMiddle,
-            flavourBottom: flavours.flavourBottom,
-          },
+            variables: {
+                to: values.to,
+                message: values.message,
+                from: values.from,
+                flavourTop: flavours.flavourTop,
+                flavourMiddle: flavours.flavourMiddle,
+                flavourBottom: flavours.flavourBottom,
+            },
         });
-    
+
         await actions.resetForm({
-          values: {
-            to: "",
-            message: "",
-            from: "",
-          },
+            values: {
+                to: "",
+                message: "",
+                from: "",
+            },
         });
-        await navigate(`/lollies/${result.data.CreateLolly?.slug}`)
+        await navigate(`/frozen/${result.data.createLolly?.slug}`)
         // await navigate(`/lollies/${result.data?.craeteLolly?.slug}`);
         console.log(result);
-      };
+    };
 
     return (
-        <div className="createlolly">
-            <Lolly
-                flavourTop={flavours.flavourTop}
-                flavourMiddle={flavours.flavourMiddle}
-                flavourBottom={flavours.flavourBottom}
-            />
-            <div className="colorSelectorContainer">
-                <label htmlFor="topFlavor" className="colorPickerLabel">
-                    <input
-                        className="colorPicker"
-                        value={flavours.flavourTop}
-                        type="color"
-                        name="topFlavor"
-                        id="topFlavor"
-                        onChange={(e) => {
-                            setFlavours({
-                                ...flavours,
-                                flavourTop: e.target.value,
-                            });
-                        }}
-                    ></input>
-                </label>
+        <header>
+            <section>
+                <aside>
+                    <Lolly
+                        flavourTop={flavours.flavourTop}
+                        flavourMiddle={flavours.flavourMiddle}
+                        flavourBottom={flavours.flavourBottom}
+                    />
+                
 
-                <label htmlFor="midFlavor" className="colorPickerLabel">
-                    <input
-                        className="colorPicker"
-                        value={flavours.flavourMiddle}
-                        type="color"
-                        name="midFlavor"
-                        id="midFlavor"
-                        onChange={(e) => {
-                            setFlavours({
-                                ...flavours,
-                                flavourMiddle: e.target.value,
-                            });
-                        }}
-                    ></input>
-                </label>
+                
+                <div>
+                    <label htmlFor="topFlavor" className="colorPickerLabel">
+                        <input
+                            className="colorPicker"
+                            value={flavours.flavourTop}
+                            type="color"
+                            name="topFlavor"
+                            id="topFlavor"
+                            onChange={(e) => {
+                                setFlavours({
+                                    ...flavours,
+                                    flavourTop: e.target.value,
+                                });
+                            }}
+                        ></input>
+                    </label>
 
-                <label htmlFor="botFlavor" className="colorPickerLabel">
-                    <input
-                        className="colorPicker"
-                        value={flavours.flavourBottom}
-                        type="color"
-                        name="botFlavor"
-                        id="botFlavor"
-                        onChange={(e) => {
-                            setFlavours({
-                                ...flavours,
-                                flavourBottom: e.target.value,
-                            });
-                        }}
-                    ></input>
-                </label>
-            </div>
+                    <label htmlFor="midFlavor" className="colorPickerLabel">
+                        <input
+                            className="colorPicker"
+                            value={flavours.flavourMiddle}
+                            type="color"
+                            name="midFlavor"
+                            id="midFlavor"
+                            onChange={(e) => {
+                                setFlavours({
+                                    ...flavours,
+                                    flavourMiddle: e.target.value,
+                                });
+                            }}
+                        ></input>
+                    </label>
+
+                    <label htmlFor="botFlavor" className="colorPickerLabel">
+                        <input
+                            className="colorPicker"
+                            value={flavours.flavourBottom}
+                            type="color"
+                            name="botFlavor"
+                            id="botFlavor"
+                            onChange={(e) => {
+                                setFlavours({
+                                    ...flavours,
+                                    flavourBottom: e.target.value,
+                                });
+                            }}
+                        ></input>
+                    </label>
+                </div>
+                </aside>
+
+                
+            
 
             <div>
-                <Formik
-                    initialValues={initialValues}
-                    onSubmit={onSubmit}
-                    validationSchema={validationSchema}
-                >
-                    <Form>
-                        <div style={{ paddingBottom: "8px" }}>
-                            <Field
-                                name="to"
-                                type="text"
-                                label="Title"
-                                className="textFeild"
-                                placeholder="to"
-                            />
-                            <ErrorMessage component={ErrorMsg} name="to" />
-                        </div>
-                        <div style={{ paddingBottom: "8px" }}>
-                            <Field
-                                style={{ resize: "none" }}
-                                className="textFeild"
-                                as="textarea"
-                                rows="3"
-                                name="message"
-                                type="text"
-                                label="Title"
-                                placeholder="Message"
-                            />
-                            <ErrorMessage component={ErrorMsg} name="message" />
-                        </div>
-                        <div style={{ paddingBottom: "12px" }}>
-                            <Field
-                                name="from"
-                                type="text"
-                                label="Title"
-                                className="textFeild"
-                                placeholder="from"
-                            />
-                            <ErrorMessage component={ErrorMsg} name="from" />
-                        </div>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={onSubmit}
+                        validationSchema={validationSchema}
+                    >
+                        <Form>
+                            <div style={{ paddingBottom: "8px" }}>
+                                <Field
+                                    name="to"
+                                    type="text"
+                                    label="Title"
+                                    className="textFeild"
+                                    placeholder="to"
+                                />
+                                <ErrorMessage component={ErrorMsg} name="to" />
+                            </div>
+                            <div style={{ paddingBottom: "8px" }}>
+                                <Field
+                                    style={{ resize: "none" }}
+                                    className="textFeild"
+                                    as="textarea"
+                                    rows="3"
+                                    name="message"
+                                    type="text"
+                                    label="Title"
+                                    placeholder="Message"
+                                />
+                                <ErrorMessage component={ErrorMsg} name="message" />
+                            </div>
+                            <div style={{ paddingBottom: "12px" }}>
+                                <Field
+                                    name="from"
+                                    type="text"
+                                    label="Title"
+                                    className="textFeild"
+                                    placeholder="from"
+                                />
+                                <ErrorMessage component={ErrorMsg} name="from" />
+                            </div>
                             <button
                                 type="submit"
                                 disabled={loading ? true : false}
                             >
                                 Freeze
                             </button>
-                    </Form>
-                </Formik>
-            </div>
-        </div>
+                        </Form>
+                    </Formik>
+                </div>
+                </section>
+        </header>
     )
 }
 
