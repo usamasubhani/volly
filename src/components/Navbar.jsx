56718952c12c9796141b4a02e01aa9b2a5f78389
Usama Subhani <reachusama@protonmail.com>
2020-11-20 00:35:38 +0500

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a href="/">Volly</a>
+            <ul>
+                <li><a href="/createlolly">Create a Volly</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
