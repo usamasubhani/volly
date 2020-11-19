1cff1b3e96c1425f3ecf9e1dade6266293efc7e2
Usama Subhani <reachusama@protonmail.com>
2020-11-20 00:36:55 +0500

    disable plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,16 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "Volly",
-        // This is the field under which it's accessible
-        fieldName: "Volly",
-        // URL to query from
-        url: "https://volly.netlify.app/.netlify/functions/volly",
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     typeName: "Volly",
+    //     // This is the field under which it's accessible
+    //     fieldName: "Vollies",
+    //     // URL to query from
+    //     url: "http://localhost:33443/.netlify/functions/volly"
+    //     // url: "https://volly.netlify.app/.netlify/functions/volly",
+    //   },
+    // },
   ],
 }
